22b351c014b117043d7484ea646bec85a101d131
Bajusz Máté <bajszmate99@gmail.com>
2020-12-29 22:15:18 +0100

    feat: added rolunk page content

--- pages/rolunk.tsx
+++ pages/rolunk.tsx
@@ -2,6 +2,12 @@ import Layout from 'containers/Layout/Layout'
 import React from 'react'
 import Image from 'next/image'
 import { Box, Grid, Typography } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import VisibilityIcon from '@material-ui/icons/Visibility'
+import ShowChartIcon from '@material-ui/icons/ShowChart'
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions'
+import PhoneInTalkIcon from '@material-ui/icons/PhoneInTalk'
+import PostAddIcon from '@material-ui/icons/PostAdd'
 
 const Rolunk = () => {
   return (
@@ -18,7 +24,6 @@ const Rolunk = () => {
         <Grid item xs={12} sm={12} md={12} lg={6}>
           <Typography variant="subtitle1">RÓLUNK</Typography>
           <hr />
-
           <Typography variant="body1">
             <Box fontWeight="fontWeightBold">
               A mestertkeresek.hu egy új internetes felület, amely összehozza a szolgáltatást nyújtó
@@ -45,10 +50,74 @@ const Rolunk = () => {
           <Box fontWeight="fontWeightBold">Megbízható mesterek, bárhol bármikor</Box>
         </Typography>
       </Grid>
-      <Grid container>
-        <Grid item xs={12} sm={12} md={4} lg={4}></Grid>
-        <Grid item xs={12} sm={12} md={4} lg={4}></Grid>
-        <Grid item xs={12} sm={12} md={4} lg={4}></Grid>
+      <Grid container spacing={8}>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
+          <Grid xs={12} sm={12} md={12} lg={12}>
+            <SearchIcon />
+          </Grid>
+          <Typography variant="h6">SZAKEMBER KERESÉS, HELY ALAPJÁN</Typography>
+          <Typography>
+            Válogass kedvedre a szakemberek közül. Ha sürgősen segítségre van szükséged fel is
+            hívhatod őket, vagy feladhatsz egy ajánlatkérést és a szakemberek keresnek meg, vagy
+            emailben értesítünk. A munka után oszd meg tapasztalataidat ismerőseiddel, segíts
+            ismerőseidnek szakembert találni.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
+          <Grid xs={12} sm={12} md={12} lg={12}>
+            <VisibilityIcon />
+          </Grid>
+          <Typography variant="h6">MESTEREK LÁTHATÓSÁGA, REKLÁM</Typography>
+          <Typography>
+            A Mestertkeresek.hu mindent megtesz a vállalkozásod fejlesztése érdekében, az elérhető
+            szolgáltatásaink alapján. Google és Facebook reklámokkal segítünk a mestereknek, illetve
+            rengeteg facebook alvállalkozói vagy szakember kereső csoportba aktívak vagyunk.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
+          <Grid xs={12} sm={12} md={12} lg={12}>
+            <ShowChartIcon />
+          </Grid>
+          <Typography variant="h6">PÉNZT HOZUNK NEKED</Typography>
+          <Typography>
+            Ha mesterként regisztrálsz itt hamar kialakulhat a fix ügyfélköröd, a szolgáltatás
+            megvásárlása után elérhető vagy az oldalon, és egyéni facebook hirdetéseinkkel pár hónap
+            alatt tucatnyi új vevőt szerezhetsz.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={8}>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
+          <Grid xs={12} sm={12} md={12} lg={12}>
+            <EmojiEmotionsIcon />
+          </Grid>
+          <Typography variant="h6">NINCS KÖZVETÍTÉS ÉS FIZETŐS AJÁNLATKÉRÉS</Typography>
+          <Typography>
+            Nálunk nem kell fizetni ajánlatkérés megtekintésért, illetve nem számolunk fel
+            közvetítési díjat. A szolgáltatás csomag megvásárlásával minden funkciót megkapsz.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
+          <Grid xs={12} sm={12} md={12} lg={12}>
+            <PhoneInTalkIcon />
+          </Grid>
+          <Typography variant="h6">EGYÉNI ELÉRHETŐSÉG TELEFONON</Typography>
+          <Typography>
+            Senki se szereti ha késő este hívják, vagy amikor más elfoglaltága van. Nálunk
+            beállíthatod hogy a hét napjain mikor tudnak hívni az emberek, egyébként csak
+            visszahívást tudnak kérni.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={12} md={4} lg={4}>
+          <Grid xs={12} sm={12} md={12} lg={12}>
+            <PostAddIcon />
+          </Grid>
+          <Typography variant="h6">MUNKA/AJÁNLATKÉRÉS FELADÁS</Typography>
+          <Typography>
+            Ha nem sürgősen kell neked szakember, létrehozhatsz egy ajánlatkérést és a mesterek
+            keresnek meg téged!
+          </Typography>
+        </Grid>
       </Grid>
     </Layout>
   )
