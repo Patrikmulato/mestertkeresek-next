b8bff4f7a633204af9919462a3d29702983af118
Patrikmulato <mulatopatrik@gmail.com>
2020-12-31 11:07:30 +0100

    feat: added keys to dotenv

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = {
   env: {
     googleMapKey: process.env.GOOGLE_API_KEY,
+    authDomain: process.env.AUTH_0_CLIENT_DOMAIN,
+    authClientId: process.env.AUTH_0_CLIENT_ID,
+    authClientSecret: process.env.AUTH_0_CLIENT_SECRET,
   },
-};
+}
--- utils/auth0.js
+++ utils/auth0.js
@@ -1,33 +1,33 @@
-import { initAuth0 } from '@auth0/nextjs-auth0';
+import { initAuth0 } from '@auth0/nextjs-auth0'
 // import config from './config';
 
 export default initAuth0({
-    domain: 'dev-x1bikxi4.eu.auth0.com',
-    clientId: 'grnPlj16eoexKKMbfmfqFteL8jIPfi4N',
-    clientSecret: 'uRWPgemwssGn3SwqKkyV8De4qPF6CJ_ck2TaQ7V6cJoFp_X--tbWGRhiHkZZueJz',
-    scope: 'openid profile',
-    redirectUri: 'http://localhost:3000/api/callback',
-    postLogoutRedirectUri: 'http://localhost:3000/',
-    session: {
-        // The secret used to encrypt the cookie.
-        cookieSecret: 'very-very-secret-token',
-        // The cookie lifetime (expiration) in seconds. Set to 8 hours by default.
-        cookieLifetime: 60 * 60 * 288,
-        // (Optional) The cookie domain this should run on. Leave it blank to restrict it to your domain.
-        cookieDomain: 'mestertkeresek.hu',
-        // (Optional) SameSite configuration for the session cookie. Defaults to 'lax', but can be changed to 'strict' or 'none'. Set it to false if you want to disable the SameSite setting.
-        cookieSameSite: 'lax',
-        // (Optional) Store the id_token in the session. Defaults to false.
-        storeIdToken: false,
-        // (Optional) Store the access_token in the session. Defaults to false.
-        storeAccessToken: false,
-        // (Optional) Store the refresh_token in the session. Defaults to false.
-        storeRefreshToken: false
-    },
-    oidcClient: {
-        // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
-        httpTimeout: 2500,
-        // (Optional) Configure the clock tolerance in milliseconds, if the time on your server is running behind.
-        clockTolerance: 10000
-    }
-});
+  domain: process.env.authDomain,
+  clientId: process.env.authClientId,
+  clientSecret: process.env.authClientSecret,
+  scope: 'openid profile',
+  redirectUri: 'http://localhost:3000/api/callback',
+  postLogoutRedirectUri: 'http://localhost:3000/',
+  session: {
+    // The secret used to encrypt the cookie.
+    cookieSecret: 'very-very-secret-token',
+    // The cookie lifetime (expiration) in seconds. Set to 8 hours by default.
+    cookieLifetime: 60 * 60 * 288,
+    // (Optional) The cookie domain this should run on. Leave it blank to restrict it to your domain.
+    cookieDomain: 'mestertkeresek.hu',
+    // (Optional) SameSite configuration for the session cookie. Defaults to 'lax', but can be changed to 'strict' or 'none'. Set it to false if you want to disable the SameSite setting.
+    cookieSameSite: 'lax',
+    // (Optional) Store the id_token in the session. Defaults to false.
+    storeIdToken: false,
+    // (Optional) Store the access_token in the session. Defaults to false.
+    storeAccessToken: false,
+    // (Optional) Store the refresh_token in the session. Defaults to false.
+    storeRefreshToken: false,
+  },
+  oidcClient: {
+    // (Optional) Configure the timeout in milliseconds for HTTP requests to Auth0.
+    httpTimeout: 2500,
+    // (Optional) Configure the clock tolerance in milliseconds, if the time on your server is running behind.
+    clockTolerance: 10000,
+  },
+})
